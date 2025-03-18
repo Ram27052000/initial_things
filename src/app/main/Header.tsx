0b8a0230fe4a43ba9013a9a734868b34642399e4
Ram.Rv <ram.rv@flender.com>
2025-03-18 11:56:06 +0530

    Welcome page added

---
+++ src/app/main/Header.tsx
@@ -0,0 +1,28 @@
+import {Code2, Github} from "lucide-react"
+import { Button } from "@/components/ui/button"
+export default function Header() {
+    return (
+        <div>
+            <header>
+                <div className="container flex justify-between">
+                    <div className="flex items-center gap-2 font-bold">
+                        <Code2 className="h-5 w-5"/>
+                        <span>R.V. Ram</span>
+                    </div>
+                    <nav className="flex items-center gap-6">
+                        <a href className="text-sm font-medium cursor-pointer hover:text-blue-500">About</a>
+                        <a href className="text-sm font-medium cursor-pointer hover:text-blue-500">Skills</a>
+                        <a href className="text-sm font-medium cursor-pointer hover:text-blue-500">Projects</a>
+                    </nav>
+                    <div className="flex-items-center">
+                        <a href="https://github.com/Ram27052000" target="_blank">
+                            <Button variant="ghost" size="icon" className="cursor-pointer hover:text-blue-500">
+                                <Github className="h-5 w-5"/>
+                            </Button>
+                        </a>
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
+}
